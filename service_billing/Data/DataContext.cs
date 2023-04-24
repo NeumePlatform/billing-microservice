1a6b7294aa5a13f233f56736c507cbe42b5c622b
stefan <452766@student.fontys.nl>
2023-04-24 11:47:39 +0200

    db

--- service_billing/Data/DataContext.cs
+++ service_billing/Data/DataContext.cs
@@ -17,7 +17,7 @@ namespace service_billing.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            Console.WriteLine(optionsBuilder.UseSqlServer("Server=neume-subscription-SQL,1433;Database=billing;User Id=sa;Password=Stefandeboer122599;"));
+            Console.WriteLine(optionsBuilder.UseSqlServer("Server=host.docker.internal,1433;Database=billing;User Id=sa;Password=Stefandeboer122599;"));
             //optionsBuilder.UseSqlServer("Server=localhost,1433;Database=billing;User Id=sa;Password=Stefandeboer122599;TrustServerCertificate=true;");
         }
 
