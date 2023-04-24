40fc02a628009b3cd093ab6c61df9cf4dc2c408a
stefan <452766@student.fontys.nl>
2023-04-24 12:34:03 +0200

    db

--- service_billing/services/TransactionService/TransactionService.cs
+++ service_billing/services/TransactionService/TransactionService.cs
@@ -24,7 +24,8 @@ namespace service_billing.services.TransactionService
         {
             Console.WriteLine("Add Transaction: " + _context.transactions.Add(transaction));
             Console.WriteLine("Transaction: " + transaction);
-            
+
+            Console.WriteLine(_context.ChangeTracker.HasChanges());
             //var response = await _context.SaveChangesAsync();
             Console.WriteLine("Save Changes: " + _context.SaveChanges());
             Console.WriteLine("Changes Saved!");
